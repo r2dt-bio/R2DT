866c4eeb420385cbb106ab7bd46ba619eca1a31a
Anton Petrov <apetrov@ebi.ac.uk>
2018-10-12 12:03:22 +0100

    Update title tags

--- docs/pdb-lsu.py
+++ docs/pdb-lsu.py
@@ -25,6 +25,7 @@ head = """
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<title>LSU rRNAs from PDB</title>
 </head>
 <body>
 <div class="container">
