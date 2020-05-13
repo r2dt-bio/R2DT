62f959de7770f26e5ee2ea1f40d64ae37ddd34ea
Anton Petrov <apetrov@ebi.ac.uk>
2020-05-13 14:45:14 +0100

    Add a script for cleaning up Rfam folder

---
+++ data/rfam/clean_up_files.sh
@@ -0,0 +1,9 @@
+find . -name *.seed -print0 | xargs -0 rm -f
+find . -name *.cov -print0 | xargs -0 rm -f
+find . -name *.power -print0 | xargs -0 rm -f
+find . -name *.surv -print0 | xargs -0 rm -f
+find . -name *.sto -print0 | xargs -0 rm -f
+find . -name *.pdf -print0 | xargs -0 rm -f
+find . -name *.svg -print0 | xargs -0 rm -f
+find . -name *.done -print0 | xargs -0 rm -f
+find . -name *.cm -print0 | xargs -0 rm -f
