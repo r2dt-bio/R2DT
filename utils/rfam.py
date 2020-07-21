eb21d35fbf0347363d3805866fcab68e8df71529
Anton Petrov <apetrov@ebi.ac.uk>
2020-07-21 12:24:12 +0100

    Better organise Rfam tRNA output files

--- utils/rfam.py
+++ utils/rfam.py
@@ -540,10 +540,12 @@ def cmsearch_nohmm_mode(fasta_input, output_folder, rfam_acc):
     Run cmsearch on the fasta sequences using cmsearch in the --nohmm mode
     to get potentially missing hits.
     """
-    subfolder = os.path.join(output_folder, 'RF00005'
+    subfolder = os.path.join(output_folder, rfam_acc)
+    os.system('mkdir -p {}'.format(subfolder))
     tblout = os.path.join(subfolder, 'cmsearch.tblout')
-    cmd = 'cmsearch --nohmm --tblout {tblout} {cm} {fasta_input}'.format(
+    cmd = 'cmsearch --nohmm -o {output} --tblout {tblout} {cm} {fasta_input}'.format(
         cm=os.path.join(config.RFAM_DATA, rfam_acc, '{}.cm'.format(rfam_acc)),
+        output=os.path.join(subfolder, 'cmsearch.out.txt'),
         tblout=tblout,
         fasta_input=fasta_input
     )
