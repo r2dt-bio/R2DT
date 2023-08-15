9c39fa309e34219514249fc5128442cb7f55d06e
Anton Petrov <anton.i.petrov@gmail.com>
2023-08-15 10:31:05 +0100

    Update ribotyper command

--- utils/shared.py
+++ utils/shared.py
@@ -40,7 +40,7 @@ def get_ribotyper_output(fasta_input, output_folder, cm_library, skip_ribovore_f
     )
     if not os.path.exists(ribotyper_long_out):
         cmd = (
-            f"ribotyper.pl --skipval -i {cm_library}/modelinfo.txt "
+            f"ribotyper --skipval -i {cm_library}/modelinfo.txt "
             f"-f {fasta_input} {output_folder} > /dev/null"
         )
         os.system(cmd)
