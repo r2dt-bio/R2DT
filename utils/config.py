f1306348d944643a9764d07779f91fa100ae3971
Anton Petrov <anton.i.petrov@gmail.com>
2025-04-22 16:40:05 +0100

    Delete old CM_LIBRARY location

--- utils/config.py
+++ utils/config.py
@@ -51,6 +51,4 @@ GTRNADB_BACT = os.path.join(DATA, "gtrnadb", "bacteria_isotype_specific")
 GTRNADB_ARCH = os.path.join(DATA, "gtrnadb", "archaea_isotype_specific")
 GTRNADB_MITO = os.path.join(DATA, "gtrnadb", "vertebrate_mitochondrial")
 
-CM_LIBRARY = os.path.join(DATA, "cms")
-
 LOCAL_DATA = os.path.join(DATA, "local_data")
