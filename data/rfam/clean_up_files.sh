366644664f32a7b5705e2765bd2c6b89daeac0a1
Anton Petrov <anton.i.petrov@gmail.com>
2025-04-27 22:01:34 +0100

    Remove RNArtist SVG preview files

--- data/rfam/clean_up_files.sh
+++ data/rfam/clean_up_files.sh
@@ -7,4 +7,5 @@ find . -name *.sto -print0 | xargs -0 rm -f
 find . -name *.pdf -print0 | xargs -0 rm -f
 find . -name *.svg -print0 | xargs -0 rm -f
 find . -name *.done -print0 | xargs -0 rm -f
+find . -name '*_rnartist.svg' -print0 | xargs -0 rm -f
 find . -name '*.cm' ! -name 'RF00005.cm' ! -name 'all.cm' -print0 | xargs -0 rm -f
